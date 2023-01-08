4bef4013e4fae797f99387d176cdc21c6bcd1ec2
Скобелев Илья <iskobelev@ppr.ru>
2023-01-08 09:02:49 +0300

    Prometheus

--- OtusCrud/Program.cs
+++ OtusCrud/Program.cs
@@ -4,6 +4,7 @@ using OtusCrud.Data;
 using OtusCrud.Models;
 using OtusCrud.Repositories;
 using OtusCrud.Services;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,12 +28,20 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
+app.UseRouting();
+app.UseHttpMetrics();
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseEndpoints(endpoint =>
+{
+    endpoint.MapMetrics();
+});
 
 app.MapGet("/health", () => new HealthResponse { Status = "OK" });
 
